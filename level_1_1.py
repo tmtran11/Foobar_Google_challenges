20060ae505c4bb4d10697526305767b011f483b5
Trang Tran <tmtran@knox.edu>
2018-02-13 20:43:56 -0600

    Create level_1_1.py

---
+++ level_1_1.py
@@ -0,0 +1,15 @@
+
+def answer(n): 
+    prime = [2]
+    p_str = "2"
+    for x in range(3,50000):
+        not_prime = False
+        for y in prime:
+            if x%y==0:
+                not_prime = True
+                break
+        if not not_prime:
+            prime.append(x)
+            p_str += str(x)
+            if len(p_str)>(n+5):
+                return str(p_str[n:n+5])
